1a22cbf3fe3541f3cd9832b8ab77dcdec91917d1
giovannamoeller <giovannagmoeller@gmail.com>
2024-12-01 13:07:34 -0300

    fix: params router

--- src/app/essays/[id]/page.tsx
+++ src/app/essays/[id]/page.tsx
@@ -1,7 +1,12 @@
 import MainLayout from '@/app/components/layout/MainLayout';
 import EssayDetailsPage from '@/app/components/essay/EssayDetailsPage';
 
-export default function Page({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+export default async function Page({ params }: Props) {
   return (
     <MainLayout>
       <EssayDetailsPage id={params.id} />
